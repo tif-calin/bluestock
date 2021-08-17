f4a94f6c1c073ec32799750ce8bb6cff114cd7bb
culi <culitif@tuta.io>
2021-08-17 02:02:20 -0700

    write test for signup route

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,8 +3,28 @@ import setup from '../data/setup.js';
 import request from 'supertest';
 import app from '../lib/app.js';
 
-describe('demo routes', () => {
+describe('auth routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
+
+  test('POST user to /auth/signup', async () => {
+    const res = await request(app)
+      .post('/api/v1/auth/signup')
+      .send({
+        username: 'test',
+        password: 'test',
+      })
+    ;
+
+    expect(res.status).toBe(200);
+    expect(res.body).toEqual({
+      id: expect.any(String),
+      username: 'test',
+      token: expect.any(String),
+      passwordHash: expect.any(String),
+      createdAt: expect.any(String)
+    });
+  });
+
 });
--- data/setup.js
+++ data/setup.js
@@ -1,6 +1,6 @@
 import { promises as fs } from 'fs';
 
-export default (pool) => {
+export default pool => {
   return fs.readFile('./sql/setup.sql', { encoding: 'utf-8' })
     .then(sql => pool.query(sql))
   ;
