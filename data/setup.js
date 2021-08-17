59c399a6871d55a30b1e27a5b967f90237159057
culi <culitif@tuta.io>
2021-08-17 02:59:31 -0700

    heroku?

--- data/setup.js
+++ data/setup.js
@@ -1,7 +1,9 @@
 import { promises as fs } from 'fs';
+import ThingService from '../lib/services/ThingService.js';
 
 export default pool => {
   return fs.readFile('./sql/setup.sql', { encoding: 'utf-8' })
     .then(sql => pool.query(sql))
+    .finally(() => ThingService.uploadData())
   ;
 };
--- setup-db.js
+++ setup-db.js
@@ -1,6 +1,4 @@
 import pool from './lib/utils/pool.js';
 import setup from './data/setup.js';
-import ThingService from './lib/services/ThingService.js';
 
 setup(pool);
-ThingService.uploadData();
