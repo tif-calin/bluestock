a791a16b1b59485934dad69140561d019b7ab879
culi <culitif@tuta.io>
2021-08-17 02:26:35 -0700

    add things route describe

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,8 +2,15 @@ import pool from '../lib/utils/pool.js';
 import setup from '../data/setup.js';
 import request from 'supertest';
 import app from '../lib/app.js';
+import ThingService from '../lib/services/ThingService.js';
 
 describe('auth routes', () => {
+
+  const user = {
+    username: 'test',
+    password: 'test',
+  };
+
   beforeEach(() => {
     return setup(pool);
   });
@@ -11,10 +18,7 @@ describe('auth routes', () => {
   test('POST user to /auth/signup', async () => {
     const res = await request(app)
       .post('/api/v1/auth/signup')
-      .send({
-        username: 'test',
-        password: 'test',
-      })
+      .send(user)
     ;
 
     expect(res.status).toBe(200);
@@ -27,4 +31,24 @@ describe('auth routes', () => {
     });
   });
 
+  test('POST user to /auth/signin', async () => {
+    const res = await request.agent(app).post('/api/v1/auth/signup').send(user)
+      .then(() => request.agent(app).post('/api/v1/auth/login').send(user))
+    ;
+
+    expect(res.status).toBe(200);
+  });
+
+});
+
+describe('things routes', () => {
+
+  beforeEach(async () => {
+    setup(pool);
+    await ThingService.uploadData();
+  });
+
+  test('', async () => {
+    
+  });
 });
