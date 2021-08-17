5810d0b62598a9c775057a93691d33ef2cbdcb54
culi <culitif@tuta.io>
2021-08-16 23:24:54 -0700

    polyfill for array.at()

--- data/data.js
+++ data/data.js
@@ -1,3 +1,5 @@
+import 'core-js';
+
 import data from './portfolio_sites.json';
 
 const filter = site => {
@@ -10,12 +12,12 @@ const munge = portfolio => {
 
   const { single_page, blog_centric, supports_https, ...rest } = portfolio;
 
-  if (single_page) tags.append('single_page');
-  if (blog_centric) tags.append('blog_centric');
-  if (supports_https) tags.append('supports_https');
-  if (rest.face) tags.append('has_face');
-  if (rest.blog) tags.append('has_blog');
-  if (rest.mail) tags.append('has_mail');
+  if (single_page) tags.push('single_page');
+  if (blog_centric) tags.push('blog_centric');
+  if (supports_https) tags.push('supports_https');
+  if (rest.face) tags.push('has_face');
+  if (rest.blog) tags.push('has_blog');
+  if (rest.mail) tags.push('has_mail');
 
   rest.tld = rest.site.split('/')[2].split('.').at(-1);
   rest.url_length = rest.site.split('//')[1].length;
